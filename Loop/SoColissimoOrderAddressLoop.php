4b5f20c07b1c5e42507e7880ad4bbb1eaad7ea17
Romain Ducher <rducher@openstudio.fr>
2015-06-17 14:55:52 +0200

    Replacing a PhpStorm header by a good licencing header.

--- Loop/SoColissimoOrderAddressLoop.php
+++ Loop/SoColissimoOrderAddressLoop.php
@@ -1,10 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ducher
- * Date: 17/06/15
- * Time: 11:18
- */
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
 
 namespace SoColissimo\Loop;
 
