6ffd24a5a6be79c767d47c79c37e9e99ceb76e4f
Vincent Lopes <vlopes@openstudio.fr>
2017-07-12 09:40:49 +0200

    Fix relais query on address with accent

--- WebService/BaseWebService.php
+++ WebService/BaseWebService.php
@@ -130,7 +130,11 @@ abstract class BaseWebService
         /*
          * Clear array
          */
-        foreach ($args as $key=>$value) {
+        foreach ($args as $key => $value) {
+            if ($key == "address" || $key == "city") {
+                $args[$key] = $this->normalize($value);
+            }
+
             if (empty($value)) {
                 unset($args[$key]);
             }
@@ -181,6 +185,31 @@ abstract class BaseWebService
             },
             $name
         );
+    }
+
+    protected function normalize($text)
+    {
+        $utf8 = array(
+            '/[áàâãªä]/u'   =>   'a',
+            '/[ÁÀÂÃÄ]/u'    =>   'A',
+            '/[ÍÌÎÏ]/u'     =>   'I',
+            '/[íìîï]/u'     =>   'i',
+            '/[éèêë]/u'     =>   'e',
+            '/[ÉÈÊË]/u'     =>   'E',
+            '/[óòôõºö]/u'   =>   'o',
+            '/[ÓÒÔÕÖ]/u'    =>   'O',
+            '/[úùûü]/u'     =>   'u',
+            '/[ÚÙÛÜ]/u'     =>   'U',
+            '/ç/'           =>   'c',
+            '/Ç/'           =>   'C',
+            '/ñ/'           =>   'n',
+            '/Ñ/'           =>   'N',
+            '/–/'           =>   '-', // UTF-8 hyphen to "normal" hyphen
+            '/[’‘‹›‚]/u'    =>   ' ', // Literally a single quote
+            '/[“”«»„]/u'    =>   ' ', // Double quote
+            '/ /'           =>   ' ', // nonbreaking space (equiv. to 0x160)
+        );
+        return preg_replace(array_keys($utf8), array_values($utf8), $text);
     }
 
     /**
